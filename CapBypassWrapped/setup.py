0c9eaf7d70bd002d9f4ac10f9ba7ad6507502bef
max <93793772+maxhithere@users.noreply.github.com>
2023-11-18 22:48:51 -0500

    update setup

--- CapBypassWrapped/setup.py
+++ CapBypassWrapped/setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='capbypasswrapped',
-    version='0.1',
-    packages=['CapBypassWrapped'],
+    version='1.1',
+    packages=find_packages(),
+    setup_requires=['setuptools_scm', 'wheel'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'requests',
         'logging'
