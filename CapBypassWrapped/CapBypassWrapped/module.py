7219c0180b0ce3972ff591916569480b662dbd49
max <93793772+maxhithere@users.noreply.github.com>
2023-11-18 22:49:22 -0500

    update main

--- CapBypassWrapped/CapBypassWrapped/module.py
+++ CapBypassWrapped/CapBypassWrapped/module.py
@@ -1,121 +1,121 @@
+
 import requests
 import logging
+import json
+import time
+from typing import Dict, Optional, Union
+
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt='[%Y-%m-%d %H:%M:%S]')
 
-class Requester:
-    def __init__(self, url):
-        self.url = url
+BASE_URL = "https://api.capbypass.com"
 
-    def make_request(self, method, data=None):
-        url = f"{self.url}"
-        if method == "GET":
-            response = requests.get(url, json=data)
-        elif method == "POST":
-            response = requests.post(url, json=data)
-        return response
+def make_request(url, method, data=None):
+    url = f"{url}"
+    if method == "GET":
+        response = requests.get(url, json=data)
+    elif method == "POST":
+        response = requests.post(url, json=data)
+    return response
 
-class CreateTask(Requester):
-    def __init__(self):
-        super().__init__("https://capbypass.com/api/createTask")
-
-        """
-
-        POST /api/createTask
-        Creates a task for solving the selected captcha type.
-
-        """
-
-    def Data(self, client_key, task_type, app_id=None, website_url=None, website_pub_key=None, website_subdomain=None, blob=None, proxy=None, invisible=False, rqdata=None, user_agent=None, verbal=False):
-        if task_type == "HCaptchaTask":
-            response = self.make_request(method="POST", data={"clientKey": client_key, "appId": app_id, "task": {"type": "HCaptchaTask", "websiteURL": website_url, "websiteKey": website_pub_key, "isInvisible": invisible, "enterprisePayload": {"rqdata": rqdata}, "userAgent": user_agent, "proxy": proxy}})
-            if response.status_code == 200:
-                if verbal:
-                    logging.info("HCaptchaTask solved successfully")
-                return response.json()
-            else:
-                if verbal:
-                    logging.warn("HCaptchaTask failed to solve")
-                return {"errorCode": response.status_code, "errorDescription": response.reason, "solved": False}
-        elif task_type == "FunCaptchaTask":
-            response = self.make_request(method="POST", data={"clientKey": client_key, "appId": app_id, "task": {"type": "FunCaptchaTask", "websiteURL": website_url, "websitePublicKey": website_pub_key, "websiteSubdomain": website_subdomain, "data[blob]": blob, "proxy": proxy}})
-            if response.status_code == 200:
-                if verbal:
-                    logging.info("FunCaptchaTask solved successfully")
-                return response.json()
-            else:
-                if verbal:
-                    logging.warn("FunCaptchaTask failed to solve")
-                return {"errorCode": response.status_code, "errorDescription": response.reason, "solved": False}
-        
-    
-class CreateClassificationTask(Requester):
-    def __init__(self,):
-        super().__init__("https://capbypass.com/api/createTask")
-
-        """
-        POST /api/createTask
-        This task will only return the correct answers of the image, and not a solved token.
-        """
-
-    def Data(self, client_key, task_type, question, queries=None, images=None, app_id=None, verbal=False):
-        if task_type == "FunCaptchaClassification":
-            response = self.make_request(method="POST", data={"clientKey": client_key, "appId": app_id, "task": {"type": "FunCaptchaClassification", "image": images, "question": question}})
-            if response.status_code == 200:
-                if verbal:
-                    logging.info("FunCaptchaClassification Task solved successfully")
-                return response.json()
-            else:
-                if verbal:
-                    logging.warn("FunCaptchaClassification Task failed to solve")
-                return {"errorCode": response.status_code, "errorDescription": response.reason, "solved": False}
-        elif task_type == "HCaptchaClassification":
-            response = self.make_request(method="POST", data={"clientKey": client_key, "appId": app_id, "task": {"type": "HCaptchaClassification", "queries": queries, "question": question}})
-            if response.status_code == 200:
-                if verbal:
-                    logging.info("HCaptchaClassification Task solved successfully")
-                return response.json()
-            else:
-                if verbal:
-                    logging.warn("HCaptchaClassification Task failed to solve")
-                return {"errorCode": response.status_code, "errorDescription": response.reason, "solved": False}
-
-class GetTaskResult(Requester):
-    def __init__(self):
-        super().__init__("https://capbypass.com/api/getTaskResult")
-
-        """
-        POST /api/getTaskResult
-        Retrieves the solution for the specified task.
-        """
-
-    def Data(self, client_key, task_id, verbal=False):
-        response = self.make_request(method="POST", data={"clientKey": client_key, "taskId": task_id})
+def create_task(
+    client_key: str,
+    task_type: str,
+    website_url: str,
+    website_pub_key: str,
+    website_subdomain: str,
+    blob: Optional[str] = None,
+    verbal: bool = False,
+    app_id: Optional[str] = None,
+) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
+    url = f"{BASE_URL}/createTask"
+    if task_type == "FunCaptchaTask":
+        dumped_blob = json.dumps({"blob": blob})
+        response = make_request(
+            url,
+            method="POST",
+            data={
+                "clientKey": client_key,
+                "appId": app_id,
+                "task": {
+                    "type": "FunCaptchaTaskProxyLess",
+                    "websiteURL": website_url,
+                    "websitePublicKey": website_pub_key,
+                    "funcaptchaApiJSSubdomain": website_subdomain,
+                    "data[blob]": dumped_blob,
+                },
+            },
+        )
         if response.status_code == 200:
             if verbal:
-                logging.info("Task result retrieved successfully")
-            return response.json()
+                logging.info("FunCaptchaTask created successfully, solving...")
+                task_id = response.json().get("taskId")
+                status = get_task_result(client_key, task_id, verbal)
+                stat = status.get("status", "failed")
+                while stat != "ready":
+                    time.sleep(0.3)
+                    status = get_task_result(client_key, task_id, verbal)
+                    stat = status.get("status", "failed")
+                    if stat == "ready":
+                        logging.info("FunCaptchaTask solved successfully")
+                        solution = status.get("solution")
+                        return {"solution": solution, "solved": True}
+                    if stat == "failed":
+                        if verbal:
+                            logging.warning(f"Failed to solve captcha: {status}")
+                        return {
+                            "errorCode": 1,
+                            "errorDescription": status,
+                            "solved": False,
+                        }
+
+            else:
+                if verbal:
+                    logging.warn("FunCaptchaTask failed to create")
+                return {
+                    "errorCode": response.status_code,
+                    "errorDescription": response.reason,
+                    "solved": False,
+                }
         else:
             if verbal:
-                logging.warn("Task result retrieval failed")
-            return {"errorCode": response.status_code, "errorDescription": response.reason}
+                logging.warning("Invalid task type")
 
-class GetBalance(Requester):
-    def __init__(self):
-        super().__init__("https://capbypass.com/api/getBalance")
+def get_task_result(
+    client_key: str,
+    task_id: str,
+    verbal: bool = False
+) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
+    url = f"{BASE_URL}/getTaskResult"
+    response = make_request(
+        url,
+        method="POST",
+        data={"clientKey": client_key, "taskId": task_id}
+    )
+    if response.status_code == 200:
+        if verbal:
+            logging.info("Task result retrieved successfully")
+        return response.json()
+    else:
+        if verbal:
+            logging.warn("Task result retrieval failed")
+        return {"errorCode": response.status_code, "errorDescription": response.reason}
 
-        """
-        POST /api/getBalance
-        Retrieves the account balance using the provided account key.
-        """
-
-    def Data(self, client_key, verbal=False):
-        response = self.make_request(method="POST", data={"clientKey": client_key})
-        if response.status_code == 200:
-            if verbal:
-                logging.info("Balance retrieved successfully")
-            return response.json()
-        else:
-            if verbal:
-                logging.warn("Balance retrieval failed")
-            return {"errorCode": response.status_code, "errorDescription": response.reason}
+def get_balance(
+    client_key: str,
+    verbal: bool = False
+) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
+    url = f"{BASE_URL}/getBalance"
+    response = make_request(
+        url,
+        method="POST",
+        data={"clientKey": client_key}
+    )
+    if response.status_code == 200:
+        if verbal:
+            logging.info("Balance retrieved successfully")
+        return response.json()
+    else:
+        if verbal:
+            logging.warn("Balance retrieval failed")
+        return {"errorCode": response.status_code, "errorDescription": response.reason}
