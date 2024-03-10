2dd6d01251d600988d6c8f983bddb1c857f5ed57
maxhithere <volodarskymax@gmail.com>
2024-03-10 15:58:45 -0400

    v2.3 changes

--- CapBypassWrapped/CapBypassWrapped/module.py
+++ CapBypassWrapped/CapBypassWrapped/module.py
@@ -34,6 +34,10 @@ class CapBypassWrapped:
             return formatted_string
         else:
             return None
+    
+    def is_base64_encoded(str):
+        regex = r'^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$'
+        return bool(re.match(regex, str))
 
     def create_task(
         self,
@@ -94,7 +98,7 @@ class CapBypassWrapped:
         else:
             if self.verbal:
                 logging.warn("Task result retrieval failed")
-            return {"errorCode": response.status_code, "errorDescription": response.text}
+            return {"errorCode": response.status_code, "errorDescription": response.json().get("errorDescription")}
 
     def get_balance(self) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
         url = f"{BASE_URL}/getBalance"
@@ -111,4 +115,79 @@ class CapBypassWrapped:
             if self.verbal:
                 logging.warn("Balance retrieval failed")
             return {"errorCode": response.status_code, "errorDescription": response.text}
+        
+    
+    def create_and_get_task_result(self, task_type: str, website_url: str, website_pub_key: str, website_subdomain: str, proxy: str, blob: Optional[str] = None, delay: int = 5) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
+        task = self.create_task(task_type, website_url, website_pub_key, website_subdomain, proxy, blob)
+        if task.get("taskId"):
+            task_id = task.get("taskId")
+            start = time.time()
+            while True:
+                task_result = self.get_task_result(task_id)
+                if task_result.get("status") == "READY":
+                    task_result["time"] = time.time() - start
+                    return task_result
+                elif time.time() - start > 200:
+                    return {"errorCode": 400, "errorDescription": "Task took too long to complete"}
+                time.sleep(delay)
+        else:
+            return task 
+        
+    def create_classification_task(
+        self,
+        task_type: str,
+        image: str,
+        question: str,
+    ) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
+        url = f"{BASE_URL}/createTask"
+        if task_type == "FunCaptchaClassification":
+            check = self.is_base64_encoded(image)
+            if not check:
+                if self.verbal:
+                    logging.warning("Image is not base64 encoded")
+                return {"errorCode": 1, "errorDescription": "Image is not base64 encoded"}
+            response = self.make_request(
+                url,
+                method="POST",
+                data={
+                    "clientKey": self.client_key,
+                    "task": {
+                        "type": "FunCaptchaClassification",
+                        "image": image,
+                        "question": question
+                    }
+                },
+            )
+            if response.status_code == 200:
+                if self.verbal:
+                    logging.info("FunCaptcha Classification Task created successfully")
+                    task_id = response.json().get("taskId")
+                    return {"taskId": task_id}
+            elif response.status_code != 200:
+                    if self.verbal:
+                        logging.warn("FunCaptcha Classification Task failed to create")
+                    return {
+                        "errorCode": response.status_code,
+                        "errorDescription": response.text,
+                    }
+            else:
+                if self.verbal:
+                    logging.warning("Invalid task type")
+                return None
+        
+    def create_and_get_classification_task_result(self, task_type: str, image: str, question: str, delay: int = 5) -> Union[Dict[str, Union[str, bool]], Dict[str, Union[int, str, bool]]]:
+        task = self.create_classification_task(task_type, image, question)
+        if task.get("taskId"):
+            task_id = task.get("taskId")
+            start = time.time()
+            while True:
+                task_result = self.get_task_result(task_id)
+                if task_result.get("status") == "READY":
+                    task_result["time"] = time.time() - start
+                    return task_result
+                elif time.time() - start > 200:
+                    return {"errorCode": 400, "errorDescription": "Task took too long to complete"}
+                time.sleep(delay)
+        else:
+            return task 
         
